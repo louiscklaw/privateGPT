741a2fd54147ba9ff7dc4ab2a3cac28d8ef189f2
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-25 16:54:35 +0800

    update privateGPT,

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ WORKDIR /app
 COPY requirements.txt /app/requirements.txt
 RUN pip3 install -r requirements.txt
 
-RUN mkdir /app/models
+# RUN mkdir /app/models
 # RUN wget https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin
-RUN wget http://192.168.10.61:8181/ggml-gpt4all-j-v1.3-groovy.bin
+# RUN wget http://192.168.10.61:8181/ggml-gpt4all-j-v1.3-groovy.bin
 
 
 # COPY main.py main.py
--- build_docker.sh
+++ build_docker.sh
@@ -22,7 +22,16 @@ set -ex
 
 # python ingest.py
 
-./fetch_docker.sh
+# ./fetch_docker.sh
+
+mkdir -p models
+cd models
+  if [[ -f "ggml-gpt4all-j-v1.3-groovy.bin" ]]; then
+    echo 'model exist, skipping download'
+  else
+    wget http://192.168.10.61:8181/ggml-gpt4all-j-v1.3-groovy.bin
+  fi
+cd -
 
 docker build . -t 192.168.10.61:5000/test-private-gpt
 docker push 192.168.10.61:5000/test-private-gpt
@@ -30,3 +39,7 @@ docker pull 192.168.10.61:5000/test-private-gpt
 
 docker compose up -d
 docker compose exec -it main bash
+
+
+docker image tag 192.168.10.61:5000/test-private-gpt logickee/test-private-gpt
+docker push logickee/test-private-gpt
